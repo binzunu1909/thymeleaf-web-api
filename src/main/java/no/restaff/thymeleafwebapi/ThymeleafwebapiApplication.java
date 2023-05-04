9326765f80b6978caad641bd320c4075e232f631
admin <hang.nguyen@restaff.no>
2023-05-04 18:03:56 +0700

    First Commit

---
+++ src/main/java/no/restaff/thymeleafwebapi/ThymeleafwebapiApplication.java
@@ -0,0 +1,13 @@
+package no.restaff.thymeleafwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafwebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafwebapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/restaff/thymeleafwebapi/ThymeleafwebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.restaff.thymeleafwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafwebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
